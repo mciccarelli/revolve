ba446f8e3003790b3cbcea21142c315ea63a2f84
michael ciccarelli <mikecicc@gmail.com>
2019-08-24 19:35:19 -0400

    add Head component with Title and default meta tags

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
+import Head from 'next/head';
 import useDimensions from 'react-use-dimensions';
-import { useViewportScroll } from 'framer-motion';
 
 import {
   Nav,
@@ -13,17 +13,16 @@ import {
 
 const HomePage = () => {
   const [ref, { y, height }] = useDimensions();
-  // const { scrollYProgress } = useViewportScroll();
-
-  // console.log('scrollYProgress', scrollYProgress);
-  // console.log('y', y);
-
   // NOTE: offset by nav height (65) and fix position
   // when y scroll progress hits bottom of hero
   const navIsSticky = height - 65 <= Math.abs(y);
 
   return (
     <div>
+      <Head>
+        <title>Revolve Law Group: A Law Firm based in Irvine, California</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <Hero childRef={ref}>
         <Nav sticky={navIsSticky} />
         <Intro />
